3ae9b7cbf6c07b8619e638f1de51ed724cc2d3dd
Luong Huy Thong <luonghuythong1994@gmail.com>
2023-05-01 10:23:19 +0700

    lap14

---
+++ WEB101x/Lap/Lap_14/script.js
@@ -0,0 +1,10 @@
+alert("Hello world");
+let country;
+let continent;
+let  polulation;
+country = 'Viet Nam';
+continent = 'Asia';
+polulation = 100000000 + ' people';
+console.log('Country: ' + country);
+console.log('Continet: ' + continent);
+console.log('polulation: ' + polulation);
