d5524fe10d935609f99539b0a7efdf7f87c04119
Luong Huy Thong <luonghuythong1994@gmail.com>
2023-05-07 16:40:55 +0700

    lap 16

---
+++ WEB101x/Lap/Lap_16/script.js
@@ -0,0 +1,61 @@
+//Lap16
+
+//Lap16.1
+// console.log('Lap 16.1');
+// console.log('9' - '5');
+// console.log('19' - '13' + '17');
+// console.log('19' - '13' + 17);
+// console.log('123' < 57);
+// console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+//Lap16.2
+// console.log('')
+// console.log('Lap 16.2');
+// let numNeighbours = prompt('How many neighbor countries does your country have?');
+// // let numNeighbours = 1;
+// if(numNeighbours == 1){
+//     console.log('Only 1 border!');
+// }else if(numNeighbours > 1){
+//     console.log('More than 1 border');
+// }else{
+//     console.log('No borders');
+// }
+
+//Lap16.3
+console.log('')
+console.log('Lap 16.3');
+let country = prompt('What is the country you choose?')
+let language = prompt('What is the language in this country??');
+let population = prompt('What is the population of the country in millions?');
+let isIsland = prompt('What is the population of this country?(yes/no)');
+if(language == 'english' && population < 50 && isIsland == 'no'){
+    console.log('You should live in ' + country);
+}else{
+    console.log(country + ' does not meet your criteria ');
+}
+
+
+
+//Lap16.4
+// console.log('')
+// console.log('Lap 16.4');
+// let language = 'China';
+// switch(language) {
+//     case 'China':
+//         console.log('MOST number of native speakers!');
+//         break;
+//     case 'Spannish':
+//         console.log('2nd place in number of native speakers');
+//         break;
+//     case 'Spannish':
+//         console.log('3rd place');
+//         break;
+//     case 'English':
+//         console.log('Number 4');
+//         break;
+//     case 'Spannish':
+//         console.log('5th most spoken language');
+//         break;
+//     default:
+//         console.log('Great language too :D');
+// }
