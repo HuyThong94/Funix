144466959347c9fd1bb602c4efc8ee1abc1d1f60
Luong Huy Thong <luonghuythong1994@gmail.com>
2023-05-29 16:58:42 +0700

    Update script.js

--- WEB101x/Lap/Lap_18/script.js
+++ WEB101x/Lap/Lap_18/script.js
@@ -3,19 +3,21 @@
 //Lap18.1
 console.log('Lap 18.1');
 const myCountry = {
-    country : 'Viet Nam',
-    capital : 'Ha Noi',
-    language : 'Vietnamese',
-    population : 100,
+    country : 'Finland',
+    capital : 'Helsinki',
+    language : 'finnish',
+    population : 6,
     neighbours : ['laos', 'China', 'Cambodia']
 }
 console.log(myCountry);
 
 console.log('Lap 18.2');
-const vietNam = myCountry['country'] + ' has ' + myCountry.population + ' million ' + myCountry.language +'-speaking people, ' + myCountry['neighbours'].length + ' neighbours coutries and capital ' + myCountry['capital'];
+const vietNam = myCountry['country'] + ' has ' + myCountry.population + ' million ' + myCountry.language +'-speaking people, ' + myCountry['neighbours'].length + ' neighbours coutries and capital called ' + myCountry['capital'];
 console.log(vietNam);
 
 myCountry.population = myCountry.population + 2;
 console.log(myCountry.population);
 myCountry['population'] = myCountry['population'] - 2;
 console.log(myCountry['population']);
+myCountry['describe'] = 'Finland has 6 million finnish-speaking people, 3 neighbouring countries and a capital called Helsinki';
+console.log(myCountry['describe']);
