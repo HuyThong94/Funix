f9e781311152aca426acdb1bee5dee9f0420c246
Luong Huy Thong <luonghuythong1994@gmail.com>
2023-06-15 22:14:16 +0700

    Update script.js

--- WEB101x/Lap/Lap_18/Lap_18.8/script.js
+++ WEB101x/Lap/Lap_18/Lap_18.8/script.js
@@ -29,3 +29,37 @@ if(john.BMI > mark.BMI){
     CompareBMI = `Mark's BMI (${mark.BMI}) is higher than John's (${john.BMI})`;
 }
 console.log(CompareBMI);
+
+//Lap18.8.1
+console.log('Lap 18.8.1');
+const bills = new Array(22, 295, 176, 440, 37, 105, 10, 110, 86, 52);
+const tips = new Array();
+const totals = new Array();
+function calcTip(bill){
+    let tip;
+    if(bill >= 50 && bill <= 300){
+        tip = bill*0.15;
+    }else{
+        tip = bill*0.2;
+    }
+    return tip;
+}
+for(let i = 0; i < bills.length; i++){
+    const tip = calcTip(bills[i]);
+    const total = tip + bills[i];
+    tips.push(tip);
+    totals.push(total);
+}
+console.log(bills);
+console.log(tips);
+console.log(totals);
+function calcAverage(totals){
+    let sum = 0;
+    for(let i = 0;i < totals.length; i++){
+        sum += totals[i];
+    }
+    const average = sum/totals.length;
+    return average;
+}
+const averageTotals = calcAverage(totals);
+console.log(averageTotals);
