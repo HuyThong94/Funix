f849afbe4aa6f7ac50e55926052b5375a8b83709
Luong Huy Thong <luonghuythong1994@gmail.com>
2024-10-04 06:07:01 +0700

    Update lap7.1.sql

---
+++ DBI202x/Lap/lap7.1.sql
@@ -0,0 +1,19 @@
+use book_data;
+
+-- Yeu cau 1
+select count(*) as number_book from books;
+
+-- Yeu cau 2
+SELECT COUNT(*) AS number_book, released_year
+FROM books
+GROUP BY released_year
+ORDER BY released_year;
+
+-- Yeu cau 3
+SELECT SUM(stock_quantity) AS sum_stock
+FROM books;
+
+-- Yeu cau 4
+SELECT AVG(released_year) AS avg_released_year
+FROM books
+GROUP BY author_lname;
