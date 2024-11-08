ddd12f116259954d3c6be1140ae75639e5a7f154
Luong Huy Thong <luonghuythong1994@gmail.com>
2024-11-08 16:19:34 +0700

    .

---
+++ DBI202x/Lap/lap12.2.sql
@@ -0,0 +1,19 @@
+use online_shop;
+
+-- B1
+start transaction;
+insert into categories set name = 'name A';
+select * from categories;
+
+-- B2
+savepoint test1;
+
+-- B3
+update categories set name = 'name B' where id = 1;
+select * from categories;
+
+-- B4
+rollback to test1;
+select * from categories;
+
+-- Khi thực hiện câu truy vấn ở b3, do chưa dùng câu truy vấn savepoint nên khi dùng câu truy vấn rollback ở b4 thì câu truy vấn ở b3 sẽ bị quay lại.
--- DBI202x/Lap/lap9.3.sql
+++ DBI202x/Lap/lap9.3.sql
@@ -73,3 +73,56 @@ JOIN max_challenge_count mcc ON cc.t_challenges = mcc.max_no_challenge
 ORDER BY cc.t_challenges DESC, cc.hacker_id;
 
 -- Yeu cau 8
+SELECT h.hacker_id, h.name, COALESCE(SUM(max_scores.max_score), 0) AS total_score
+FROM Hackers h
+JOIN (
+    SELECT hacker_id, challenge_id, MAX(score) AS max_score
+    FROM Submissions
+    GROUP BY hacker_id, challenge_id
+) AS max_scores ON h.hacker_id = max_scores.hacker_id
+GROUP BY h.hacker_id, h.name
+HAVING total_score > 0
+ORDER BY total_score DESC, h.hacker_id ASC;
+
+-- Yeu cau 9
+SELECT 
+    MIN(Start_Date), 
+    MAX(End_Date) 
+    FROM 
+    (SELECT *, DATE_ADD(Start_Date, INTERVAL - DENSE_RANK() OVER ( ORDER BY Start_Date) DAY) AS Grp FROM Projects) B 
+    group by grp 
+    order by MAX(End_Date)-MIN(Start_Date);
+    
+-- Yeu cau 10
+SELECT s.Name
+FROM Students AS s
+LEFT JOIN Friends AS f 
+ON s.ID = f.ID
+LEFT JOIN Packages AS p_stu
+ON s.ID = p_stu.ID
+LEFT JOIN Packages AS p_fri
+ON f.Friend_ID = p_fri.ID
+WHERE  p_fri.Salary > p_stu.Salary
+ORDER BY p_fri.Salary ASC;
+
+-- Yeu cau 11
+select distinct f.x, f.y 
+from functions f
+where f.x < f.y and exists(select x, y from functions where f.x = y and f.y = x) or 
+      f.x = f.y and (select count(x) from functions where x = f.x and x = y) > 1
+order by f.x;
+
+-- Yeu cau 12
+SELECT c.contest_id, c.hacker_id, c.name,
+       COALESCE(SUM(s.total_submissions), 0) AS total_submissions,
+       COALESCE(SUM(s.total_accepted_submissions), 0) AS total_accepted_submissions,
+       COALESCE(SUM(v.total_views), 0) AS total_views,
+       COALESCE(SUM(v.total_unique_views), 0) AS total_unique_views
+FROM Contests c
+JOIN Colleges col ON c.contest_id = col.contest_id
+JOIN Challenges ch ON col.college_id = ch.college_id
+LEFT JOIN View_Stats v ON ch.challenge_id = v.challenge_id
+LEFT JOIN Submission_Stats s ON ch.challenge_id = s.challenge_id
+GROUP BY c.contest_id, c.hacker_id, c.name
+HAVING total_submissions > 0 OR total_accepted_submissions > 0 OR total_views > 0 OR total_unique_views > 0
+ORDER BY c.contest_id;
