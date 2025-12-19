dbb7195f3960894e6ef929ca602576a5e22172e6
Luong Huy Thong <luonghuythong1994@gmail.com>
2025-12-19 16:21:11 +0700

    .

---
+++ IOT301/Lap/lap_3.1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    * This is Lab No. 2, created by Nguyen Van A
+    * Date of creation: xxx
+    * Date of modified: yyy
+    * Goal of this project: Input and output data using scanf and printf
+    */
+    printf("Name: Thong Luong Huy\n");
+    printf("DOB: May 14, 1994\n");
+    printf("Mobile: 0968888888\n");
+    return 0;
+}
---
+++ IOT301/Lap/name/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char strName[20];
+    printf("My name is Luong Huy Thong\n");
+    printf("Enter your name: ");
+    scanf("%s", &strName);
+    printf("Your name is %s\n", strName);
+    return 0;
+}
