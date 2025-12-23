34e74ccb465a6c4d67305d54f4080cc9e8f522dd
Luong Huy Thong <luonghuythong1994@gmail.com>
2025-12-24 05:54:55 +0700

    .

--- IOT301/Lap/Bai12.1_Lap12/main.c
+++ IOT301/Lap/Bai12.1_Lap12/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    char str[] = "Luong Huy Thong";
+    char *ptr = str;
+    int i = 0;
+
+    // Bước 2: Tính độ dài chuỗi bằng con trỏ
+    while (*(ptr + i) != '\0') {
+        i++;
+    }
+    i--; // đưa i về vị trí ký tự cuối cùng
+
+    // Bước 3: In chuỗi theo thứ tự ngược
+    for (; i >= 0; i--) {
+        printf("%c", *(ptr + i));
+    }
+
     return 0;
 }
